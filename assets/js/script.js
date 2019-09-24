8df9c97019f6aa31af674ea12f54be1e06f9c8fa
smita151 <smitagaikwad1312@gmail.com>
2019-09-24 17:41:22 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,31 +15,43 @@
   $(function () {
   $("#datepicker").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+         calendarWeeks: true
   }).datepicker('update', new Date());
+
 });
 
   $(function () {
   $("#datepicker1").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
 
-   $(function () {
-  $("#datepicker2").datepicker({ 
-        autoclose: true, 
-        todayHighlight: true
-  }).datepicker('update', new Date());
-});
+//    $(function () {
+//   $("#datepicker2").datepicker({ 
+//         autoclose: true, 
+//         todayHighlight: true,
+//        showWeek: true
+//   }).datepicker('update', new Date());
+// });
 
+$(function () {
+$("#datepicker2").datepicker({ 
+autoclose: true, 
+todayHighlight: true,
+calendarWeeks: true
+}).datepicker('update', new Date());
+});
 
 
  $(function () {
   $("#datepicker3").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
@@ -47,7 +59,8 @@
   $(function () {
   $("#datepicker4").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
@@ -56,245 +69,280 @@
  $(function () {
   $("#datepicker5").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker6").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker7").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker8").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker9").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker10").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker11").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker12").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker13").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker14").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker15").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker16").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker17").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker18").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker19").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker20").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker21").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker22").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker23").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker24").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker25").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker26").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker27").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker28").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker29").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker30").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker31").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker32").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker33").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker34").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker35").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker36").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker37").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
  $(function () {
   $("#datepicker38").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
 $(function () {
   $("#datepicker39").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
@@ -302,7 +350,8 @@ $(function () {
 $(function () {
   $("#datepicker40").datepicker({ 
         autoclose: true, 
-        todayHighlight: true
+        todayHighlight: true,
+        calendarWeeks: true
   }).datepicker('update', new Date());
 });
 
--- assets/plugins/bootstrap-4.1.2/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-/*!
-  * Bootstrap v4.1.2 (https://getbootstrap.com/)
-  * Copyright 2011-2018 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
-  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-  */(function (global,factory){typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery'), require('popper.js')):typeof define === 'function' && define.amd ? define(['exports','jquery','popper.js'],factory) : (factory((global.bootstrap = {}),global.jQuery,global.Popper))}(this,(function (exports,$,Popper){$ = $ && $.hasOwnProperty('default') ? $['default']:$;Popper = Popper && Popper.hasOwnProperty('default') ? Popper['default']:Popper},)($); exports.Util = Util; exports.Alert = Alert; exports.Button = Button; exports.Carousel = Carousel; exports.Collapse = Collapse; exports.Dropdown = Dropdown; exports.Modal = Modal; exports.Popover = Popover; exports.Scrollspy = ScrollSpy; exports.Tab = Tab; exports.Tooltip = Tooltip; Object.defineProperty(exports,'__esModule'{value:true})));
-//# sourceMappingURL=bootstrap.js.map
-__ESCAPED_SOURCE_END_CLEAN_CSS__
